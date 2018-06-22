64779fb45124c6a07dd321cf9125cebed375803f
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-06-22 22:24:44 +0530

    Create int1.py

---
+++ int1.py
@@ -0,0 +1,14 @@
+def main():
+a=int(input("enter the n integer:"))
+a[n]={2,3,4,5}
+print(a)
+b=int(input("enter the k integer:"))
+b[k]={1,4,5,6}
+print(b)
+for i in range n
+print a[i]
+for i in range k
+print b[k]
+s=0
+s=s+a[i]
+print("sum of the first num is "s,k)
